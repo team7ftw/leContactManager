86643339d78c6a78177e9714861d7c36a45a1631
Dev-Khan <cww904@gmail.com>
2020-01-28 18:25:47 -0500

    Only connect to Database no Cursor
    Trying to nail down the issues. Currently it could be the cursor.

--- application.py
+++ application.py
@@ -38,7 +38,7 @@ db_connect = pyodbc.connect('DRIVER='+driver+
 '''
 
 db_conn = pymssql.connect(server='team7ftw.database.windows.net', user='admins@team7ftw.database.windows.net', password='#cop4331', database='ContactManager')
-cursor = db_conn.cursor()
+#cursor = db_conn.cursor()
 
 app = Flask(__name__)
 #api = Api(app)
