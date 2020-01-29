8e79dbf32561aa690ee022f107dc35ad16e85d4f
Conner <cweik@knights.ucf.edu>
2020-01-28 21:49:25 -0500

    commit

--- application.py
+++ application.py
@@ -9,16 +9,6 @@ import pymssql
 import pyodbc
 
 
-
-
-"""
-Helpful Gubs:
-	https://kite.com/blog/python/flask-restful-api-tutorial/
-	
-	https://docs.microsoft.com/en-us/sql/connect/python/pymssql/step-3-proof-of-concept-connecting-to-sql-using-pymssql?view=sql-server-ver15
-	
-"""
-
 server = 'team7ftw.database.windows.net'
 database = 'ContactManager'
 username = 'admins'
@@ -26,14 +16,6 @@ password = '#cop4331'
 
 #conn = psycopg2.connect(database=database, user=username, password=password, host=server, port="1433")
 #cursor = conn.cursor()
-
-'''
-try:
-	db_conn = pymssql.connect(server='team7ftw.database.windows.net', user='admins', password='#cop4331', database='ContactManager')
-	cur = db_conn.cursor()
-except Exception as e:
-	print(e)
-'''
 
 app = Flask(__name__)
 #api = Api(app)
@@ -49,25 +31,12 @@ def test():
 def testDB():
 	try:
 		# db_conn = pymssql.connect(server='team7ftw.database.windows.net', user='admins@team7ftw', password='#cop4331', database='ContactManager')
-		#cur = db_conn.cursor()
+		# cur = db_conn.cursor()
 		
 		db_conn = pyodbc.connect('DRIVER={SQL Server};SERVER={0};DATABASE={1};UID={2};PWD={3}'.format(server, database, username, password))
 		return "Success"
 	except Exception as e:
 		return "Reached except block:\n" + str(e)
-
-		'''
-@app.route('/db2', methods=['GET'])
-def testDB2():
-	
-		db_conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER={0};DATABASE={1};UID={2};PWD={3}'.format(server, database, username, password))
-		cur = db_conn.cursor()
-		
-		cursor.execute("INSERT INTO dbo.UserLogin (login_un, login_pw) VALUES (CW, 1234);")
-		cursor.execute("SELECT * FROM dbo.UserLogin WHERE login_un='CW'")
-		
-		return jsonify(cursor)
-	 '''
 
 @app.route('/Users', methods= ['GET', 'PUT', 'POST', 'DELETE'])
 def userFunctions():
@@ -134,7 +103,7 @@ def userContact():
 	
 	if request.method == 'GET':
 
-		"""DATABASE TO GET CONTACT DATA"""
+		# DATABASE TO GET CONTACT DATA
 		
 		return jsonify({'contact': {'No Contacts': {'phone': '(xxx)xxx-xxxx'}}})
 		
@@ -143,7 +112,7 @@ def userContact():
 		address  = request.args.get()
 		# OTHER DATA
 		
-		"""DATABASE CALL TO INSERT NEW CONTACT"""
+		# DATABASE CALL TO INSERT NEW CONTACT
 		
 		return "Success"
 		
@@ -152,12 +121,12 @@ def userContact():
 		address  = request.args.get()
 		# OTHER DATA
 		
-		"""DATABASE CALL TO UPDATE CONTACT"""
+		# DATABASE CALL TO UPDATE CONTACT
 		
 		return "Success"
 		
 	elif request.method == 'DELETE':
-		"""DATABASE CALL TO REMOVE CONTACT"""
+		# DATABASE CALL TO REMOVE CONTACT
 		return "Success"
 		
 	
