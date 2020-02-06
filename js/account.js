cd0b836b3674af0884e18ed06de08751902d0e20
Nick <nick.evans57@gmail.com>
2020-02-06 11:09:38 -0500

    Check if usernames are taken on update page

--- js/account.js
+++ js/account.js
@@ -1,42 +1,64 @@
-const currentUser = localStorage.getItem('currentUser');
-const currentUsername = localStorage.getItem('currentUsername');
+const currentUser = localStorage.getItem("currentUser");
+const currentUsername = localStorage.getItem("currentUsername");
 
-// Hide toasts so they don't cover UI while hidden
-$('.toast').hide();
-$('.toast').on('hidden.bs.toast', function() {
-   $('.toast').hide(); 
+// Prevent hidden toasts from covering UI elements
+$(".toast").hide();
+$(".toast").on("hidden.bs.toast", function() {
+    $(".toast").hide();
 });
 
 $(document).ready(function() {
-    $('#currentUsername').val(currentUsername);
-    $('#newUsername').val(currentUsername);
+    $("#currentUsername").val(currentUsername);
+    $("#newUsername").val(currentUsername);
 
-    $('main').hide().removeClass('hidden').fadeIn();
+    // Fade in after rendering
+    $("main")
+        .hide()
+        .removeClass("hidden")
+        .fadeIn();
 });
 
-$('#updateUser').click( () => {
-    const newUsername = $('#newUsername').val();
+$("#updateUser").click(() => {
+    const newUsername = $("#newUsername").val();
 
-    fetch('https://cop4331group7api.azurewebsites.net/users', {
-        method: 'POST',
+    // Check if username is taken
+    fetch("https://cop4331group7api.azurewebsites.net/dev/showtable/users")
+        .then(res => {
+            return res.text();
+        })
+        .then(data => {
+            // Fetch usernames from string
+            const usernames = /row = \d ([a-zA-Z]*) /gm;
+
+            const matches = data.matchAll(usernames);
+            for (const m of matches) {
+                if (m[1] === currentUsername) alert("User already exists!");
+                return false;
+            }
+        });
+
+    fetch("https://cop4331group7api.azurewebsites.net/users", {
+        method: "POST",
         body: JSON.stringify({
-            'currentUsername': currentUsername,
-            'newUsername': newUsername,
-            'newPassword': $('#newPass').val()
+            currentUsername: currentUsername,
+            newUsername: newUsername,
+            newPassword: $("#newPass").val()
         })
     }).then(res => {
-        $('#successToast').show().toast("show");
-        $('#currentUsername').val(newUsername);
-        $('#newUsername').val('');
-        $('#currentPass').val('');
-        $('#newPass').val('');
+        $("#successToast")
+            .show()
+            .toast("show");
+        $("#currentUsername").val(newUsername);
+        $("#newUsername").val("");
+        $("#currentPass").val("");
+        $("#newPass").val("");
 
-        localStorage.setItem('currentUsername', newUsername);
+        localStorage.setItem("currentUsername", newUsername);
     });
 });
 
 function logout(e) {
     e.preventDefault();
-    localStorage.removeItem('currentUser');
-    window.location = 'index.html';
+    localStorage.removeItem("currentUser");
+    window.location = "index.html";
 }
