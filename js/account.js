831d70dbf1b5143a9f624ddd6463e8f5a62941f0
NickEvans <nick.evans57@gmail.com>
2020-02-06 15:51:12 -0500

    Finish user auth

--- js/account.js
+++ js/account.js
@@ -31,34 +31,37 @@ $("#updateUser").click(() => {
         })
         .then(data => {
             // Fetch usernames from string
-            const usernames = /row = \d ([a-zA-Z]*) /gm;
+            if(newUsername != currentUsername) {
+                const usernames = /row = \d ([a-zA-Z]*) /gm;
 
-            const matches = data.matchAll(usernames);
-            for (const m of matches) {
-                if (m[1] === currentUsername) alert("User already exists!");
-                return false;
+                const matches = data.matchAll(usernames);
+                for (const m of matches) {
+                    if (m[1] === currentUsername) alert("User already exists!");
+                    return false;
+                }
             }
-        });
-
-    fetch("https://cop4331group7api.azurewebsites.net/users", {
-        method: "POST",
-        body: JSON.stringify({
-            'currentUsername': currentUsername,
-            'newUsername': newUsername,
-            'newPassword': md5($("#newPass").val() + salt),
-            'newSalt': salt
+        }).then(() => {
+            fetch("https://cop4331group7api.azurewebsites.net/users", {
+                method: "POST",
+                body: JSON.stringify({
+                    'currentUsername': currentUsername,
+                    'newUsername': newUsername,
+                    'newPassword': md5($("#newPass").val() + salt),
+                    'newSalt': salt
+                })
+            }).then(res => {
+                $("#successToast")
+                    .show()
+                    .toast("show");
+                $("#currentUsername").val(newUsername);
+                $("#newUsername").val("");
+                $("#currentPass").val("");
+                $("#newPass").val("");
+        
+                localStorage.setItem("currentUsername", newUsername);
+            });
         })
-    }).then(res => {
-        $("#successToast")
-            .show()
-            .toast("show");
-        $("#currentUsername").val(newUsername);
-        $("#newUsername").val("");
-        $("#currentPass").val("");
-        $("#newPass").val("");
-
-        localStorage.setItem("currentUsername", newUsername);
-    });
+    
 });
 
 function logout(e) {
